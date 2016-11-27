8c635e6f36693afa058f140fcc0bbdc5387532be
sfox-1 <seth@technometro.com>
2016-11-26 22:50:30 -0700

    Updated so it can store parsed emails in the database.

--- webemcrawler/webemcrawler/newy.py
+++ webemcrawler/webemcrawler/newy.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import sqlite3
 import urllib
 import itertools
+import re
 
 
 def bruteforce(charset, maxlength):
@@ -18,8 +19,14 @@ conn.text_factory = str
 
 cur.executescript('''
 
-CREATE TABLE IF NOT EXISTS Valid (
+CREATE TABLE IF NOT EXISTS Domain (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
       URL TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Email (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+      email TEXT UNIQUE
 )
 ''')
 url = list(bruteforce('abcdefghijklmnopqrstuvwxyz', 2))
@@ -30,12 +37,18 @@ for urls in url:
         urls = 'http://{0}.{1}'.format(urls, 'com')
         bs = BeautifulSoup(urllib.urlopen(urls).read(), "html.parser")
         if 'FailedURI' in str(bs):
-            print urls, 'dead link or empty domain!'
+            print urls, "dead link or empty domain!"
             continue
         else:
-            print urls, "THIS ONE WORKS AWESOME"
-            cur.execute('''INSERT OR IGNORE INTO Valid (URL) VALUES ( ? )''',
+            print urls, "Is a valid and working site"
+            cur.execute('''INSERT OR IGNORE INTO Domain (URL) VALUES ( ? )''',
                         (urls, ))
+            emails = re.findall('[aA-zZ]+?@[aA-zZ]+[.][aA-zZ]+', str(bs))
+            for mail in emails:
+                print mail, "going into database"
+                cur.execute(
+                    '''INSERT OR IGNORE INTO Email (email) VALUES ( ? )''',
+                    (mail, ))
             conn.commit()
             continue
     except Exception as e:
